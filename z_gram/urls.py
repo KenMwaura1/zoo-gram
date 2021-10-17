192b81e12e20183554cc7e432f0f0c476dc1729d
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-18 02:05:30 +0300

    Updated app urls for urls, registration, profiles search and posts

--- z_gram/urls.py
+++ z_gram/urls.py
@@ -1,9 +1,18 @@
 from django.urls import include, path
-
+from z_gram.views import LikePost
 from z_gram import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('post/<id>', views.post_comment, name='comment'),
+    path('post/<id>/like', LikePost.as_view(), name='liked'),
+    path('like', views.like_post, name='like_post'),
+    path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
+    path('follow/<to_follow>', views.follow, name='follow'),
+    path('search/', views.search_profile, name='search'),
 ]
