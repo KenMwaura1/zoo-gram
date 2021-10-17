f782e02599e25f7205b5dec1aa0470d1f8e8515d
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-17 16:10:18 +0300

    Created user model and migrations

--- z_gram/models.py
+++ z_gram/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """
+    model for the user including related methods
+    """
+    user = models.OneToOneField(User, related_name="userprofile", on_delete=models.CASCADE)
+    username = models.TextField(max_length=150)
+    bio = models.CharField(max_length=400, blank=True, null=True, default="simple bio")
+    profile_picture = models.ImageField(upload_to='images/profile', default='user.png')
+    location = models.CharField(max_length=70, blank=True, null=True)
+
+    @classmethod
+    @receiver(post_save, sender=User)
+    def create_user_profile(cls, sender, instance, created, **kwargs):
+        """
+        receives signal once user model is saved, if user was created we create a UserProfile instance
+        """
+        if created:
+            cls.objects.create(user=instance)
+
+    @staticmethod
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return f'{self.user.username}: profile'
+
+    @classmethod
+    def search_profile(cls, search_term):
+        """
+        method to search for users
+        """
+        return cls.objects.filter(user__username__icontains=search_term).all()
+
+
