22c3f35f438a696510e90905b02495c8c29908fb
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-17 17:06:03 +0300

    Created user_post model and migrations

--- z_gram/models.py
+++ z_gram/models.py
@@ -1,3 +1,4 @@
+import blank as blank
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
@@ -11,7 +12,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name="userprofile", on_delete=models.CASCADE)
     username = models.TextField(max_length=150)
     bio = models.CharField(max_length=400, blank=True, null=True, default="simple bio")
-    profile_picture = models.ImageField(upload_to='images/profile', default='user.png')
+    profile_picture = models.ImageField(upload_to='images/profile/', default='user.png')
     location = models.CharField(max_length=70, blank=True, null=True)
 
     @classmethod
@@ -44,4 +45,37 @@ class UserProfile(models.Model):
         """
         return cls.objects.filter(user__username__icontains=search_term).all()
 
+
+class UserPost(models.Model):
+    """
+    model for post by User
+    """
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='userposts')
+    image = models.ImageField(upload_to='images/posts/')
+    caption = models.CharField(max_length=250, blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    name = models.CharField(max_length=250, blank=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ["-pk"]
+
+    def get_absolute_url(self):
+        return f"/post/{self.id}"
+
+    @property
+    def get_all_comments(self):
+        return self.comments.all()
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return f'{self.user.username} Post'
 
