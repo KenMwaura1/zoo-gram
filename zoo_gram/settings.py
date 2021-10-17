042f5608c8247bbfd0dc9d8477f47ab60de318a6
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-17 15:27:36 +0300

    Updated settings for static files

--- zoo_gram/settings.py
+++ zoo_gram/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+    'z_gram.apps.ZGramConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+# configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
