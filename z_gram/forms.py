567023a94613950c9e3a929547f887556539e0f4
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-17 21:29:46 +0300

    created the profile and user_profile routes

--- z_gram/forms.py
+++ z_gram/forms.py
@@ -16,29 +16,33 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
-    class UpdateUserProfileForm(forms.ModelForm):
-        """
-        class for updating user profile
-        """
-        class Meta:
-            model = UserProfile
-            fields = ['username', 'location', 'profile_picture', 'bio']
 
-    class UpdateUserForm(forms.ModelForm):
-        """
-        form for updating a user details
-        """
-        email = forms.EmailField(max_length=120, help_text='Required. Enter a valid email address.')
+class UpdateUserProfileForm(forms.ModelForm):
+    """
+    class for updating user profile
+    """
 
-        class Meta:
-            model = User
-            fields = ('username', 'email')
+    class Meta:
+        model = UserProfile
+        fields = ['username', 'location', 'profile_picture', 'bio']
+
+
+class UpdateUserForm(forms.ModelForm):
+    """
+    form for updating a user details
+    """
+    email = forms.EmailField(max_length=120, help_text='Required. Enter a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
 
 
 class UserPostForm(forms.ModelForm):
     """
     form for creating new posts
     """
+
     class Meta:
         model = UserPost
         fields = ('image', 'caption')
@@ -48,6 +52,7 @@ class CommentForm(forms.ModelForm):
     """
     form to comment on posts
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['comment'].widget = forms.TextInput()
--- z_gram/views.py
+++ z_gram/views.py
@@ -2,12 +2,11 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
-from z_gram.forms import RegisterForm, UserPostForm
-from z_gram.models import UserPost
-
+from z_gram.forms import RegisterForm, UserPostForm, UpdateUserForm, UpdateUserProfileForm
+from z_gram.models import UserPost, Follow
 
 """def home(request):
     return render(request, 'z-gram/home.html')"""
@@ -30,8 +29,8 @@ def register(request):
 
 @login_required(login_url='login')
 def home(request):
-    images = UserPost.objects.all()
-    users = User.objects.exclude(id=request.user.id)
+    all_images = UserPost.objects.all()
+    all_users = User.objects.exclude(id=request.user.id)
     if request.method == 'POST':
         form = UserPostForm(request.POST, request.FILES)
         if form.is_valid():
@@ -42,9 +41,50 @@ def home(request):
     else:
         form = UserPostForm()
     params = {
-        'images': images,
+        'images': all_images,
+        'users': all_users,
         'form': form,
-        'users': users,
-
     }
     return render(request, 'z-gram/home.html', params)
+
+
+@login_required(login_url='login')
+def profile(request, username):
+    images = request.user.userprofile.userposts.all()
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateUserProfileForm(instance=request.user.userprofile)
+    params = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'images': images,
+
+    }
+    return render(request, 'z-gram/profile.html', params)
+
+
+@login_required(login_url='login')
+def user_profile(request, username):
+    user_profile = get_object_or_404(User, username=username)
+    if request.user == user_profile:
+        return redirect('profile', username=request.user.username)
+    user_posts = user_profile.userprofile.userposts.all()
+    followers = Follow.objects.filter(followed=user_profile.userprofile)
+    follow_status = None
+    for follower in followers:
+        follow_status = request.user.userprofile == follower.follower
+    params = {
+        'user_prof': user_profile,
+        'user_posts': user_posts,
+        'followers': followers,
+        'follow_status': follow_status
+    }
+    print(followers)
+    return render(request, 'z-gram/user_profile.html', params)
