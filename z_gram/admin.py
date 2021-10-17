3c3c6702b6d682b5a5ed5fdae7fc92e691a9a7fb
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-17 17:35:12 +0300

    Updated admin panel

--- z_gram/admin.py
+++ z_gram/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.models import User
+
+from z_gram.models import UserComment, Follow, UserPost, UserProfile
+
+admin.register(UserComment)
+admin.register(Follow)
+admin.register(UserPost)
+admin.register(UserProfile)
