2012dfca0f705f7fba175edd818e6a55360236d2
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-17 17:24:09 +0300

    Created user and profile update forms

--- z_gram/forms.py
+++ z_gram/forms.py
@@ -16,3 +16,14 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+    class UpdateUserProfileForm(forms.ModelForm):
+        class Meta:
+            model = UserProfile
+            fields = ['username', 'location', 'profile_picture', 'bio']
+
+    class UpdateUserForm(forms.ModelForm):
+        email = forms.EmailField(max_length=120, help_text='Required. enter a valid email address.')
+
+        class Meta:
+            model = User
+            fields = ('username', 'email')
