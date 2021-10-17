539413f1bb70dce4cb0cfd013509e04e5480f8f2
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-17 18:28:24 +0300

    created route for user registration

--- z_gram/views.py
+++ z_gram/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
 
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from z_gram.forms import RegisterForm
+
+
 def home(request):
     return render(request, 'z-gram/home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'django_registration/registration_form.html', {'form': form})
