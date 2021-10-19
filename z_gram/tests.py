92cd2a6a5fa19139dfd4575227263b274afed07b
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-19 16:51:05 +0300

    wrote tests for the posts model

--- z_gram/tests.py
+++ z_gram/tests.py
@@ -23,3 +23,30 @@ class TestProfile(TestCase):
         after = UserProfile.objects.all()
         self.assertTrue(len(after) > 0)
 
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.user = User(username='zoo-codes')
+        self.user.save()
+        self.profile_test = UserProfile(id=3, username='zoo-test', user=self.user)
+        # self.profile_test.save()
+
+        self.image_test = UserPost(image='user.png', caption='default test', user=self.profile_test)
+
+    def tearDown(self):
+        UserPost.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, UserPost))
+
+    def test_save_image(self):
+        self.image_test.save()
+        images = UserPost.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image_test.save()
+        self.image_test.delete()
+        after = UserPost.objects.all()
+        self.assertTrue(len(after) < 1)
